f97ab7a1088f03552792d4307aab0b3e578f184b
HugoMeloo <142102368+HugoMeloo@users.noreply.github.com>
2024-08-20 21:25:54 -0300

    Gerenciamento de Estoque
    Projeto Final

---
+++ ProjetoInicial1_3/src/model/Tipo.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author hugog
+ */
+public class Tipo {
+
+    String novoTipo;
+
+    public String getNovoTipo() {
+        return novoTipo;
+    }
+
+    public void setNovoTipo(String novoTipo) {
+        this.novoTipo = novoTipo;
+    }
+
+}
